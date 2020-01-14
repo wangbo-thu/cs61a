ee78388212f593a34062a81c25a51f89b5e841ed
wangbo <wangbo_thu@163.com>
2020-01-14 17:06:07 +0800

    download lab

--- download_hw.py
+++ download_hw.py
@@ -3,17 +3,20 @@ import os
 import bs4
 import re
 
-dirName = 'hw'
+dirName = 'lab'
 os.makedirs(dirName, exist_ok=True)
-# url = 'https://cs61a.org/hw/'
-url = 'https://cs61a.org/hw/sol-'
+url = 'https://cs61a.org/'
+# url = 'https://cs61a.org/hw/sol-'
 
-for i in range(1, 11):
+for i in range(4, 15):
     # link = 'hw{:02d}/hw{:02d}.zip'.format(i, i)
-    link = 'hw{:02d}/hw{:02d}.py'.format(i, i)
+    link = '{}/{}{:02d}/{}{:02d}.zip'.format(dirName, dirName, i, dirName, i)
     res = requests.get(url + link)
     print('Downloading: {}'.format(os.path.basename(link)))
-    res.raise_for_status()
+    try:
+        res.raise_for_status()
+    except Exception as ex:
+        print(ex.msg)
     fileName = os.path.join(dirName, os.path.basename(link))
     with open(fileName, 'wb') as f:
         f.write(res.content)
